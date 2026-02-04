a932213ddeb49bed9eaba221fec9ad2bf086e98a
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2026-02-04 17:32:49 +0200

    add app strings

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,65 @@
+abstract class AppStrings {
+  static const String appName = 'Dalel';
+  static const String Skip = 'Skip';
+  static const String Next = 'Next';
+  static const String Explore_The_history_with_Dalel_in_a_smart_way =
+      'Explore The history with Dalel in a smart way';
+  static const String
+      Using_our_apps_history_libraries_you_can_find_many_historical_periods =
+      "Using our app’s history libraries you can find many historical periods";
+  static const String From_every_place_on_earth = "From every place on earth";
+  static const String A_big_variety_of_ancient_places_from_all_over_the_world =
+      "A big variety of ancient places from all over the world";
+  static const String Using_modern_AI_technology_for_better_user_experience =
+      "Using modern AI technology for better user experience";
+  static const String
+      AI_provide_recommendations_and_helps_you_to_continue_the_search_journey =
+      "AI provide recommendations and helps you to continue the search journey";
+  static const String Login_Now = "Login Now";
+  static const String Welcome = "Welcome !";
+  static const String I_have_agree_to_our = "I have agree to our";
+  static const String Terms_and_Conditions = "Terms and Conditions";
+  static const String Create_Account = "Create Account";
+  static const String Sign_Up = "Sign Up";
+  static const String Already_have_an_account = "Already have an account ?";
+  static const String Sign_In = "Sign In";
+  static const String Welcome_Back = "Welcome Back !";
+  static const String Email_Address = "Email Address";
+  static const String First_Name = "First Name";
+  static const String Last_Name = "Last Name";
+  static const String Password = "Password";
+  static const String Forgot_Password = "Forgot Password ?";
+  static const String Historical_periods = "Historical periods";
+  static const String Ancient_Wars = "Ancient Wars";
+  static const String Recommendations = "Recommendations";
+  static const String Recent = "Recent";
+  static const String Profile = "Profile";
+  static const String Notification = "Notification";
+  static const String Edit_Profile = "Edit Profile";
+  static const String General = "General";
+  static const String Settings = "Settings";
+  static const String Security = "Security";
+  static const String Bazar = "Bazar";
+  static const String My_Cart = "My_Cart";
+  static const String Total_Payment = "Total Payment";
+  static const String History_Books = "History Books";
+  static const String Checkout_Now = "Checkout Now";
+  static const String Checkout_Screen = "Checkout Screen";
+  static const String Delivery_Address = "Delivery Address";
+  static const String Selected_Product = "Selected Product";
+  static const String Payment_Method = "Payment Method";
+  static const String Change = "Change";
+  static const String items = "items";
+  static const String Historical_Souvenirs = "Historical Souvenirs";
+  static const String Privacy_Policy = "Privacy Policy";
+  static const String Confirm_Payment = "Confirm Payment";
+  static const String Back_To_Home = "Back To Home";
+  static const String Log_Out = "Log Out";
+  static const String Order_Success = "Order Success";
+  static const String
+      Thank_you_for_your_order_here_and_your_package_will_be_sent_to_your_address_very_quickly_and_fast_good_product =
+      "Thank you for your order here and your package will be sent to your address very quickly and fast good product";
+
+  static const String Search_deep_into_the_history =
+      "Search deep into the history";
+}
